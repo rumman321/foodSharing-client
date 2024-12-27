e5f7efcb520f679dbeb020e664045b6e15ca42bd
rumman321 <cset-210652@dti.ac>
2024-12-27 19:18:59 +0600

    get data user email wise

---
+++ src/commponents/FoodManageTab.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const FoodManageTab = () => {
+  return (
+    <div>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Job</th>
+              <th>Favorite Color</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            <tr>
+              <td>Cy Ganderton</td>
+              <td>Quality Control Specialist</td>
+              <td>Blue</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default FoodManageTab;
--- src/pages/ManageFood.jsx
+++ src/pages/ManageFood.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import axios from "axios";
+import React, { useContext, useEffect, useState } from "react";
+import FoodManageTab from "../commponents/FoodManageTab";
+import { AuthContext } from "../providers/AuthProvider";
 
 const ManageFood = () => {
-    return (
-        <div>
-            Manage food
-        </div>
-    );
+    const {user}=useContext(AuthContext)
+  const [foods, setFoods] = useState([]);
+  useEffect(() => {
+    fetchAllFood();
+  }, []);
+  const fetchAllFood = async () => {
+    //{data} korar karon axios akta obj return kore er vitor data namok obj sob data thake
+    const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/food/${user?.email}`);
+    setFoods(data);
+    console.log(data)
+  };
+  return (
+    <div className="pt-20 w-11/12 mx-auto">
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>Food Name</th>
+              <th>foodQuantity</th>
+              <th>Favorite Color</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            {foods.map((food) => (
+              <tr key={food._id}>
+                <td>{food.foodName}</td>
+                <td>{food.foodQuantity}</td>
+                <td>Blue</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default ManageFood;
